9389e1a4f988b1a5f9424c940a3f7ccd48c86a63
AlarconCode <julian.alarcon81@gmail.com>
2023-11-01 11:58:24 +0100

    ready to deploy disable function storage local multer

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,27 +1,27 @@
-import multer from "multer";
-import {dirname, extname, join} from 'path';
-import { fileURLToPath } from "url";
+// import multer from "multer";
+// import {dirname, extname, join} from 'path';
+// import { fileURLToPath } from "url";
 
-export const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
-console.log(CURRENT_DIR);
-const MIMETYPES = ['image/jpeg', 'image/png', 'image/jpg']
+// export const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
+// console.log(CURRENT_DIR);
+// const MIMETYPES = ['image/jpeg', 'image/png', 'image/jpg']
 
-export const multerUpload = multer({
-  storage: multer.diskStorage({
-    destination: join(CURRENT_DIR, '../../uploads'),
-    filename: (req, file, cb) => {
-      const fileExtension = extname(file.originalname)
-      const fileName = file.originalname.split(fileExtension)[0]
+// export const multerUpload = multer({
+//   storage: multer.diskStorage({
+//     destination: join(CURRENT_DIR, '../../uploads'),
+//     filename: (req, file, cb) => {
+//       const fileExtension = extname(file.originalname)
+//       const fileName = file.originalname.split(fileExtension)[0]
 
-      cb(null, `${fileName}-${Date.now()}${fileExtension}`)
-    }
-  }),
-  fileFilter: (req, file, cb) => {
-    if (MIMETYPES.includes(file.mimetype)) cb(null, true)
-    else cb(new Error(`Only ${MIMETYPES.join(' ')} are allowed`))
-  },
-  limits: {
-    fieldSize: 10000000
-  }
-}); 
+//       cb(null, `${fileName}-${Date.now()}${fileExtension}`)
+//     }
+//   }),
+//   fileFilter: (req, file, cb) => {
+//     if (MIMETYPES.includes(file.mimetype)) cb(null, true)
+//     else cb(new Error(`Only ${MIMETYPES.join(' ')} are allowed`))
+//   },
+//   limits: {
+//     fieldSize: 10000000
+//   }
+// }); 
  
