0a6f3fb0b2f09342f12f23c26d8e35874c24da10
AlarconCode <julian.alarcon81@gmail.com>
2023-11-03 19:12:02 +0100

    delete credentials true from cors app.js

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.set('PORT', process.env.PORT || 4000)
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-  credentials: true
-}))
+app.use(cors())
 app.use('/api', productRouter)
 app.use('/api', authRouter)
 // app.use('/public', express.static(join(CURRENT_DIR, '../../uploads')))
