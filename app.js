ae496e64c02bc7037a013a44bbfd8f4f77dc22f3
AlarconCode <julian.alarcon81@gmail.com>
2023-11-03 18:28:48 +0100

    update config multer storage cloudinary v2

--- app.js
+++ app.js
@@ -4,28 +4,21 @@ import express from 'express'
 const app = express()
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-// import {join} from 'path'
 import productRouter from './src/routes/products.routes.js'
 import authRouter from './src/routes/auth.routes.js'
 import { cloudinaryConfig } from './src/libs/cloudinary.js'
 // import { CURRENT_DIR } from './src/libs/storage.js'
-// import fileUpload from 'express-fileupload'
 
 // MiddleWares
 app.set('PORT', process.env.PORT || 4000)
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cookieParser())
-// app.use(fileUpload({
-  //   useTempFiles: true,
-  //   tempFileDir: './uploads'
-  // }))
-  app.use(cors({
-    credentials: true
-  }))
-  app.use('/api', productRouter)
-  app.use('/api', authRouter)
-  app.use('*', cloudinaryConfig)
+app.use(cors({
+  credentials: true
+}))
+app.use('/api', productRouter)
+app.use('/api', authRouter)
 // app.use('/public', express.static(join(CURRENT_DIR, '../../uploads')))
 
 export default app
